0dea383b2a7983139cb9504b5bb2d24cbbd415e3
raffyorbe <8110851+raffyorbe@users.noreply.github.com>
2026-03-25 17:44:44 -0400

    Update jobs.py
    Transferred logic to services
    Used models where appropriate

--- routes/jobs.py
+++ routes/jobs.py
@@ -1,16 +1,15 @@
 from fastapi import APIRouter, HTTPException, Query, Depends
 from typing import Optional
-from models import JobCreate, JobResponse, JobsListResponse
+from models import JobCreate, JobUpdate, JobsListResponse
 from models_db import Job
-from utils import find_job
+from utils import find_job, success_response, error_response
 from database import sessionLocal # Import from database.py
 from sqlalchemy.orm import Session
+from services.job_service import get_all_jobs, get_job, create_job, update_job, delete_job
 
 router = APIRouter()
 
-
 # Database
-# jobs = []
 def get_db():
     db = sessionLocal() # manages the DB connection/session
     try:
@@ -18,12 +17,10 @@ def get_db():
     finally:
         db.close()
 
-
 # READ all
 @router.get("/jobs")
-def get_jobs(
+def get_jobs_route(
     db: Session = Depends(get_db), # uses resulting db session into endpoint automatically
-
     completed: Optional[bool] = Query(default=None),
     title: Optional[str] = Query(default=None),
     skip: int = Query(default=0), # how many records to skip
@@ -31,72 +28,36 @@ def get_jobs(
     sort: Optional[str] = Query(default=None), #what value to use for sorting
     order: Optional[str] = Query(default="asc"), #ascending or descending order for sorting
 ):
-    query = db.query(Job)
-
-    # Step 1 - FILTER
-    if completed is not None:
-        query = query.filter(Job.completed == completed)
-
-    if title is not None:
-        query = query.filter(Job.title.ilike(f"%{title}%"))
-
-    # Step 2 - SORT
-    descending = (order or "asc").lower() == "desc" # Bool 1 if == "desc"
-
-    if sort:
-        column = getattr(Job, sort, None) # Get column within the Job model that matches whatever the value of sort is. Else, none (in case sort value is ivalid).
-        if column is not None:
-            if descending: # If bool descending is 1
-                query = query.order_by(column.desc())
-            else:
-                query = query.order_by(column.asc())
     
-    total = query.count() # Total of all filtered results - not after paginating which only returns total on the page
-
-    # Step 3 - PAGINATE
-    results =  query.offset(skip).limit(limit).all() # offset(skip) - start at index [skip]; limit(limit) - limit to (limit) number of rows; all() gets everything w/ or w/o filter sort paginate conditions
-    
-    return {
-        "total": total,
-        "count": len(results),
-        "data": results
-        }
+    jobs = get_all_jobs(db, completed, title, skip, limit, sort, order)
+    return success_response(jobs)
 
 # READ id
 @router.get("/jobs/{job_id}")
-def get_job(job_id: int, db: Session = Depends(get_db)): 
-    job = db.query(Job).filter(Job.id == job_id).first() #Access db.query(Job) table, find the rows where Job.id (in table) is same as job_id (input) - return only one item first() instead of a list all()
+def get_job_route(job_id: int, db: Session = Depends(get_db)): 
+    job = get_job(job_id, db) #Access db.query(Job) table, find the rows where Job.id (in table) is same as job_id (input) - return only one item first() instead of a list all()
     if not job:
-        return {"error": "Job not found"} # Always {"key": "value"}
-    return job
+        return error_response(404, "Job not found")
+    return success_response(job)
 
 # CREATE
 @router.post("/jobs") 
-def create_job(title: str, completed: bool, db: Session = Depends(get_db)): # This will shift back to include job: JobCreate because frontend sends JSON with data structure, not individual query paramaeters title completed etc.
-    new_job = Job(title = title, completed = completed) # in Job, title takes title (str) and completed takes completed (bool)
-    db.add(new_job) # Add new_job entry into db
-    db.commit() # Save the entry
-    db.refresh(new_job) # Pulls the latest data from db which might include auto generated id number
-    return new_job
+def create_job_route(job: JobCreate, db: Session = Depends(get_db)): # This will shift back to include job: JobCreate because frontend sends JSON with data structure, not individual query paramaeters title completed etc.
+    new_job = create_job(job, db)
+    return success_response(new_job, "Job created successfully")
 
 # UPDATE
 @router.put("/jobs/{job_id}")
-def update_job(job_id: int, title: str, completed: bool, db: Session = Depends(get_db)): 
-    job = db.query(Job).filter(Job.id == job_id).first()
+def update_job_route(job_id: int, job: JobUpdate, db: Session = Depends(get_db)): 
+    job = update_job(job_id, job, db)
     if not job:
-        return {"error": "Job not found"}
-    job.title = title # Ignore error in syntax in IDE because at runtime, job will already exist
-    job.completed = completed
-    db.commit()
-    db.refresh(job)
-    return job
+        return error_response(404, "Job not found")
+    return success_response(job)
 
 # DELETE
 @router.delete("/jobs/{job_id}")
-def delete_job(job_id: int, db: Session = Depends(get_db)):
-    job = db.query(Job).filter(Job.id == job_id).first()
+def delete_job_route(job_id: int, db: Session = Depends(get_db)):
+    job = delete_job(job_id, db)
     if not job:
-        return {"error": "Job not found"}
-    db.delete(job)
-    db.commit()
-    return {"message": "Job deleted"}
+        return error_response(404, "Job not found")
+    return success_response(job, "Job deleted successfully")

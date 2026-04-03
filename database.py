dc48722c85b5e01e7673d8c75decfe31062dbd34
raffyorbe <8110851+raffyorbe@users.noreply.github.com>
2026-04-02 20:04:56 -0400

    Updated for PostgreSQL

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./jobs.db"
+DATABASE_URL = "postgresql://my_jobs_db_user:skC1huhCOVqzN4rfBxQT7WlRgJFGiF3Y@dpg-d77fr2s50q8c73cq3r1g-a.virginia-postgres.render.com/my_jobs_db"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}) # Connects to SQLite
+engine = create_engine(DATABASE_URL) # Connects to PostgreSQL, SQLite specific connect_args={"check_same_thread": False} was removed
 
 sessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False) # Talks to DB
 
